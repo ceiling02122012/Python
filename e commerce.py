39422742e5bda90261a2507166cf70a9ef6d9423
ceiling02122012 <misopunika@gmail.com>
2023-02-25 18:29:29 +0700

    my first python project

---
+++ e commerce.py
@@ -0,0 +1,35 @@
+print ("Welcome to e commerce")
+print ("Pick a category:")
+print ("1. school supply")
+print ("2. sanitary")
+print ("3. furniture")
+print ("4. books")
+print ("5. shoes")
+print ("6. clothes")
+option=input()
+if option=="1":
+    print("1. eraser")
+    print("2. pencil")
+    print("3. notepad")
+elif option=="2":
+    print("1. tissue")
+    print("2. soap")
+    print("3. mug")
+elif option=="3":
+    print("1. chairs")
+    print("2. table")
+    print("3. sofa")
+elif option=="4":
+    print("1. fiction")
+    print("2. non-fiction")
+    print("3. comic")
+elif option=="5":
+    print("1. nike")
+    print("2. addidas")
+    print("3. skecher")
+elif option=="6":
+    print("1. uniqlo")
+    print("2. H&M")
+    print("3. Polo")
+else:
+    print("Please pick the right option.")
